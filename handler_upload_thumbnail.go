32dd10aa2925378fc7261e3b83c6f95c99bc2382
Johnermac <ermac.rodrigues@protonmail.com>
2025-09-30 00:29:42 -0300

    thumbnail update

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
 	"github.com/google/uuid"
 )
+
 
 func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Request) {
 	videoIDString := r.PathValue("videoID")
@@ -30,8 +32,62 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 
 
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
+	
+	const maxMemory = 10 << 20
 
-	// TODO: implement the upload here
+	if err := r.ParseMultipartForm(maxMemory); err != nil {
+		http.Error(w, "could not parse multipart form", http.StatusBadRequest)
+		return
+	}
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	file, header, err := r.FormFile("thumbnail"); 
+	if err != nil {
+    http.Error(w, "could not read file from form", http.StatusBadRequest)
+		return
+	}
+	defer file.Close()
+
+	mediaType := header.Header.Get("Content-Type")
+
+	data, err := io.ReadAll(file)
+	if err != nil {
+    http.Error(w, "could not read file", http.StatusBadRequest)
+		return
+	}
+
+	video, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+    http.Error(w, "could not get the video", http.StatusBadRequest)
+		return
+	}
+
+	if video.UserID != userID {
+		respondWithError(w, http.StatusUnauthorized, "Unauthorized", err)
+		return
+	}
+
+	videoThumbnails[video.ID] = thumbnail{
+		data: data,
+		mediaType: mediaType,
+	}
+
+	
+	thumbnailURL := fmt.Sprintf("http://localhost:%s/api/thumbnails/%s", cfg.port, videoID)
+	
+	video.ThumbnailURL = &thumbnailURL
+	fmt.Println("thumb URL:", thumbnailURL)
+	
+	err = cfg.db.UpdateVideo(video)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Could not update the video", err)
+		return
+	}
+
+	video, err = cfg.db.GetVideo(videoID)
+	if err != nil {
+    http.Error(w, "could not get the video", http.StatusBadRequest)
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, video)
 }
